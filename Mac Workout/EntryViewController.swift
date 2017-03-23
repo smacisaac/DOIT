0565f7264105cf7906a679ae193a97a667113241
Seamus MacIsaac <seamus.macisaac@gmail.com>
2017-03-23 12:17:53 -0500

    First commit

---
+++ Mac Workout/EntryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EntryViewController.swift
+//  Mac Workout
+//
+//  Created by Seamus MacIsaac on 2017-03-22.
+//  Copyright © 2017 Seamus MacIsaac. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    @IBOutlet weak var menuOutletLabel1: UILabel!
+    var menuName = "NOEMOJI"
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuOutletLabel1.text = menuName
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    
+    /*
+  COMMENTS
+    */
+
+}
--- Mac Workout/ViewController.swift
+++ Mac Workout/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView1: UITableView!
+    
+    var menuSelectionItems = ["Thing 1","Thing 2","Thing 3","Thing 4","Thing 5",]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView1.dataSource = self
+        tableView1.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuSelectionItems.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = menuSelectionItems[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let menuSelection = menuSelectionItems[indexPath.row]
+        performSegue(withIdentifier: "moveSegue1", sender: menuSelection)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let defVC1 = segue.destination as! EntryViewController
+        defVC1.menuName = sender as! String
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
